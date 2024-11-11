19fc0c5d2018dec2a6e0075d4554a256bb6a4d56
Nsengiyumva Wilberforce <nsengiyumvawilberforce@gmail.com>
2024-11-11 09:44:08 +0300

    new update

--- lib/services/ApiService.dart
+++ lib/services/ApiService.dart
@@ -2,11 +2,7 @@ import 'package:http/http.dart' as http;
 import 'package:ippu/Util/app_endpoints.dart';
 
 class ApiService {
-<<<<<<< HEAD
-  static const String baseUrl = 'https://ippu.org/api'; // Define the base URL
-=======
   static const String baseUrl = AppEndpoints.baseUrl;
->>>>>>> apis
 
   Future<String> fetchData(String endpoint) async {
     final response = await http.get(Uri.parse('$baseUrl/$endpoint'));
